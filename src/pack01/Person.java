d6984c6c1c98f6b4fa9b42ac07d0f331d1f7da58
DinurikaShalini <dinurika.s99@gmail.com>
2021-02-17 14:20:22 +0530

    Addional classes added

---
+++ src/pack01/Person.java
@@ -0,0 +1,7 @@
+package pack01;
+
+public class Person {
+	public static void main(String args[]) {
+		System.out.println("Second class created");
+	}
+}
---
+++ src/pack01/Person01.java
@@ -0,0 +1,7 @@
+package pack01;
+
+public class Person01 {
+	public static void main(String args[]) {
+	 System.out.println("Changes done");
+}
+}
